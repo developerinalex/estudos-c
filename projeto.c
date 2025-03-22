bb684aa85ecb52a9bc7a0d2cf69e4371e21fb2b1
allinux <alextrusting1@gmail.com>
2025-03-22 08:53:28 -0300

    fiz um pequeno exercicio para calcular media no projeto.c

--- projeto.c
+++ projeto.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
-int main()
+float media(float a, float b, float c, float d);
+
+int main(int argc, char *argv[])
 {
-    char nome[20];
-    int v[4];
+    float a,b,c,d,m;
+    printf("Calculando a média entre 4 números.\n");
+    printf("Digite o primeiro número: ");
+    scanf("%f", &a);
+    printf("Digite o segundo número: ");
+    scanf("%f", &b);
+    printf("Digite o terceiro número: ");
+    scanf("%f", &c);
+    printf("Digite o quarto número: ");
+    scanf("%f", &d);
 
-    printf("Seja bem-vindo. Que tal começarmos pelo seu nome? ");
-    scanf("%s", nome);
+    m = media(a,b,c,d);
+    printf("media: %.2f\n", m);
+}
 
-    printf("É um prazer, %s. Agora, crie uma senha de 4 digítos: ", nome);
-    scanf("%d", v);
-
+float media(float a, float b, float c, float d){
+    return((a + b + c + d) / 4);
 }
