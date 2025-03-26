46a3d25d734108e8270f4693877b3454539dc32a
allinux <alextrusting1@gmail.com>
2025-03-26 07:21:59 -0300

    elaborando código

--- desafios-do-gpt/palindromos.c
+++ desafios-do-gpt/palindromos.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+char palin(char p);
+
 int main(int argc, char *argv[]){
     setlocale(LC_ALL, "Portuguese");
 
@@ -10,9 +12,24 @@ int main(int argc, char *argv[]){
     printf("Vamos verificar se a sua frase ou palavra é um Palíndromo. Vocẽ tem um limite de 100 caracteres!\n Digite aqui: ");
     scanf("%100[^\n]", input);
 
-    printf("%ld\n", strlen(input));
+    char verify;
+
+    verify = palin(input);
 }
 
 char palin(char p){
+    int in = strlen(p);
+    int i = 0;
+    char on[101], result[101] = p;
 
+    for(i = 0; i < in; i++){
+        result[101] = on[101];
+    }
+
+    if(result == p){
+        printf("%s", result);
+    }
+    else{
+        printf("A palavra não é um palíndromo.");
+    }
 }
